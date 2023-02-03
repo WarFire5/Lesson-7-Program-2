4d1f04cd984c02b90c4d1187034f1f871ffc4bed
Татьяна <@WIN-E362SIULNK8>
2023-02-03 23:37:44 +0300

    Добавьте файлы проекта.

---
+++ Lesson 7 Program 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7_Program_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string A, B;
+
+            Console.WriteLine("Задайте число 'A':");
+            A = Console.ReadLine();
+
+            Console.WriteLine("Задайте число 'B':");
+            B = Console.ReadLine();
+
+            int a = Convert.ToInt32(A);
+
+            int b = Convert.ToInt32(B);
+
+            int addition = a + b;
+
+            int multiplication = a * b;
+
+            int subtraction = a - b;
+
+            if (a > b)
+            {
+                Console.WriteLine(addition);
+            }
+
+            if (a == b)
+            {
+                Console.WriteLine(multiplication);
+            }
+
+            if (a < b)
+            {
+                Console.WriteLine(subtraction);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
